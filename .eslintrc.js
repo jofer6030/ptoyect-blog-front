261aee4dfaa2d67ea43e90530f22b6fc43d5eb5d
jose fernando <jofer0628@gmail.com>
2022-04-21 13:02:12 -0500

    proyect home-header avanzado

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['warn', 2],
+    'no-unused-vars': 'warn',
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+  },
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <header className='home__header'>
+      <h2>Startup Framework. Suit Up Your Startup</h2>
+      <p>
+        We have created a new product that will help designers, developers and
+        companies create websites for their startups quickly and easily.
+      </p>
+
+      <div className='home__header__buttons'>
+        <button>Get Started</button>
+        <button>Learn More</button>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
